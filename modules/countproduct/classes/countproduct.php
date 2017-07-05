895c68781f14c8fcc430556962eb8c4c44d9e044
karina BRANTE <karina.brante@gmail.com>
2017-07-05 16:44:55 +0200

    init countproduct

---
+++ modules/countproduct/classes/countproduct.php
@@ -0,0 +1,51 @@
+<?php
+class CountProduct extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'CountProduct';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0';
+        $this->author = 'keiko';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('countproduct');
+        $this->description = $this->l('Count Product');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+    public function install()
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        if (!parent::install() ||
+        !$this->registerHook('leftColumn') ||
+        !$this->registerHook('header') ||
+        !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+    ) {
+        return false;
+    }
+
+    return true;
+}
+public function uninstall()
+{
+    if (!parent::uninstall() ||
+    !Configuration::deleteByName('MYMODULE_NAME')
+) {
+    return false;
+}
+
+return true;
+}
+}
